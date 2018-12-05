580c5e6eeca07e8bcc047e76a2ba14c78b9dd86c
Anthony Abidakun <toni@vitalclick.co.za>
2018-12-05 14:50:56 +0200

    Update the Welcome Message
    Change the welcome message for the Bikeshare project file, bikeshare.py.
    
    Resolves: Line #19

--- bikeshare.py
+++ bikeshare.py
@@ -16,7 +16,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('\n------------------------------------------------------------------------')
-    print('Hello! This is Anthony. Let\'s explore some US bikeshare data!\n')
+    print('Hi! Welcome to my Bikeshare Project. Let\'s explore some of this data together!\n')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         try:
